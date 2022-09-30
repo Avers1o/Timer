0b70cf586efd7393fc60c240658d9703ce76217f
Avers1o <atlant1696@mail.ru>
2022-10-01 00:32:03 +0300

    Getting Time components

---
+++ js/script.js
@@ -0,0 +1,69 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+
+
+    // Tabs.
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            // item.style.display = 'none'; // inline-стили.
+            item.classList.add('hide'); // css-стили.
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        // tabsContent[i].style.display = 'block'; // inline-стили.
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
+
+    // Timer.
+
+    const deadline = '2022-10-14';
+
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(total / (24 * 60 * 60 * 1000)),
+            hours = Math.floor((total / (60 * 60 * 1000) % 24)),
+            minutes = Math.floor((total / (60 * 1000) % 60)),
+            seconds = Math.floor((total / 1000) % 60);
+
+        return {
+            'total': total,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+});
