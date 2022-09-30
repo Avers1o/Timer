a8681765356fa17b82314b14e076a83e51cfd59d
Avers1o <atlant1696@mail.ru>
2022-10-01 00:36:56 +0300

    Creating a timer function

--- js/script.js
+++ js/script.js
@@ -67,12 +67,38 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
 
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
     function setClock(selector, endtime) {
 
         const timer = document.querySelector(selector),
             days = timer.querySelector("#days"),
             hours = timer.querySelector('#hours'),
             minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds');
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const time = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(time.days);
+            hours.innerHTML = getZero(time.hours);
+            minutes.innerHTML = getZero(time.minutes);
+            seconds.innerHTML = getZero(time.seconds);
+
+            if (time.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
     }
+
+    setClock('.timer', deadline);
 });
